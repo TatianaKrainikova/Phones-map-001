46b40e91e9d8ace143c13f2931f059080aec83e5
Tanya <krajnikova2004@gmail.com>
2023-08-05 19:59:41 +0100

    Create app controller

---
+++ src/controllers/AppController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import models.AppModel;
+import views.AppView;
+
+public class AppController {
+
+    AppModel model;
+    AppView view;
+
+    public AppController(AppModel model, AppView view) {
+        this.model = model;
+        this.view = view;
+    }
+}

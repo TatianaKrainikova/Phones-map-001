f979424fc0e9f8fa9bcbf86b8b7725e99ff13363
Tanya <krajnikova2004@gmail.com>
2023-08-05 16:41:03 +0100

    Create choice model

---
+++ src/models/AppModel.java
@@ -0,0 +1,15 @@
+package models;
+
+public class AppModel {
+
+    private int choice;
+
+    public int getChoice() {
+        return choice;
+    }
+
+    public void setChoice(int choice) {
+        this.choice = choice;
+    }
+
+}

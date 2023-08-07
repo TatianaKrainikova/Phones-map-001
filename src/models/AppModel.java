18cc65cd616d291088cb03a941ac1444b3e6621f
Tanya <krajnikova2004@gmail.com>
2023-08-07 17:06:31 +0100

    Add methods

--- src/models/AppModel.java
+++ src/models/AppModel.java
@@ -1,5 +1,10 @@
 package models;
 
+import controllers.ContactCreateController;
+import controllers.ContactReadController;
+import views.ContactCreateView;
+import views.ContactReadView;
+
 public class AppModel {
 
     private int choice;
@@ -12,4 +17,17 @@ public class AppModel {
         this.choice = choice;
     }
 
+    public void addContacts() {
+        ContactCreateModel model = new ContactCreateModel();
+        ContactCreateView view = new ContactCreateView(model);
+        ContactCreateController controller= new ContactCreateController(model, view);
+        controller.addContact();
+    }
+
+    public void readContacts() {
+        ContactReadModel model = new ContactReadModel();
+        ContactReadView view = new ContactReadView(model);
+        ContactReadController controller = new ContactReadController(model, view);
+        controller.getContacts();
+    }
 }

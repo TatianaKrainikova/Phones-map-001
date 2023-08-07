d4496135d4ac74cbc33b07793d9a9b2f17e8ece8
Tanya <krajnikova2004@gmail.com>
2023-08-07 16:48:15 +0100

    Add controller for reading all contacts

---
+++ src/controllers/ContactReadController.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import models.ContactReadModel;
+import views.ContactReadView;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ContactReadController {
+
+    ContactReadModel model;
+    ContactReadView view;
+
+    public ContactReadController(ContactReadModel model, ContactReadView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void getContacts() {
+        view.getOutput(readContacts());
+    }
+
+    public String readContacts() {
+        Map<String, String> map = model.readContacts();
+        AtomicInteger count = new AtomicInteger(0);
+        StringBuilder stringBuilder = new StringBuilder();
+        map.forEach((key, value) ->
+                stringBuilder.append(count.incrementAndGet())
+                        .append(") ")
+                        .append(key)
+                        .append(" ")
+                        .append(value)
+                        .append("\n")
+        );
+        return stringBuilder.toString();
+    }
+
+    //TODO
+}

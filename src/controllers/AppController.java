32483c3ae508f37590094a052dd3b80fc66e6c34
Tanya <krajnikova2004@gmail.com>
2023-08-07 17:09:11 +0100

    Add choice filtration method

--- src/controllers/AppController.java
+++ src/controllers/AppController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.AppModel;
+import utils.Constants;
 import views.AppView;
 
 public class AppController {
@@ -11,5 +12,20 @@ public class AppController {
     public AppController(AppModel model, AppView view) {
         this.model = model;
         this.view = view;
+    }
+    public void runApp() {
+        filterChoice(view.doChoice());
+    }
+
+    private void filterChoice(int choice) {
+        switch (choice) {
+            case 1 -> model.addContacts();
+            case 2 -> model.readContacts();
+            case 0 -> {
+                String output = Constants.CLOSE_APP_MSG;
+                view.getOutput(output, choice);
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + choice);
+        }
     }
 }
